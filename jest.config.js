f73714f21160270659a2845b0fe655b43865afb8
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2018-03-10 01:00:36 +0000

    Hoisted module files to root level

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testMatch: [
+    "<rootDir>/src/**/*.spec.js",
+    "<rootDir>/src/**/*.spec.jsx"
+  ],
+  setupFiles: [
+    "<rootDir>/src/setupTests.js"
+  ],
+  moduleNameMapper: {
+  }
+};
